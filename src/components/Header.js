e5441e4df7e6f89c71c56778a4df8ae3b2903bc4
Demontier <demontier@outlook.com>
2022-01-09 17:19:46 -0300

    adiciona email ao header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import './Header.css';
 
 export default function Header() {
+  const email = useSelector((state) => state.login.email);
+
   return (
     <header className="conteiner-header">
-      <div>Sistema Financeiro</div>
-      <div>Filtrar</div>
-      <div>Nome Completo</div>
+      <h1>Carteira</h1>
+      <p>{email}</p>
     </header>
   );
 }
